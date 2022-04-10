719d6f59ed4e2893ef1e3a324e97ce5335721295
Michael Ovdiienko <mishaov@gmail.com>
2022-04-10 22:19:46 +0300

    feat: add up button

--- js/images-appear.js
+++ js/images-appear.js
@@ -1,18 +1,18 @@
-const firstItem = "            <a class=\"item\">\n" +
+const firstItem = new Object("            <a class=\"item\">\n" +
     "                <img src=\"img/1.png\" alt=\"\" draggable=\"false\">\n" +
     "                <p><span id=\"more1\">This is the start page of the Currency Bot.</span></p>\n" +
     "                <button onclick=\"myFunction(more1, myBtn1)\" id=\"myBtn1\">Show text</button>\n" +
-    "            </a>";
-const secondItem = "            <a class=\"item\">\n" +
+    "            </a>");
+const secondItem = new Object("            <a class=\"item\">\n" +
     "                <img src=\"img/2.png\" alt=\"\" draggable=\"false\">\n" +
     "                <p><span id=\"more2\">Description of the currency bot</span></p>\n" +
     "                <button onclick=\"myFunction(more2, myBtn2)\" id=\"myBtn2\">Show text</button>\n" +
-    "            </a>";
-const thirdItem = "            <a class=\"item\">\n" +
+    "            </a>");
+const thirdItem = new Object("            <a class=\"item\">\n" +
     "                <img src=\"img/3.png\" alt=\"\" draggable=\"false\">\n" +
     "                <p><span id=\"more3\">Example of the usage of the currency bot</span></p>\n" +
     "                <button onclick=\"myFunction(more3, myBtn3)\" id=\"myBtn3\">Show text</button>\n" +
-    "            </a>";
+    "            </a>");
 
 const items = [firstItem, secondItem, thirdItem];
 

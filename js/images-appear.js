ef8f89f314c7eb120e607c46f60934d5c82ab5f9
Michael Ovdiienko <mishaov@gmail.com>
2022-04-10 19:02:43 +0300

    feat: wip

--- js/images-appear.js
+++ js/images-appear.js
@@ -1,39 +1,39 @@
 const firstItem = "            <a class=\"item\">\n" +
     "                <img src=\"img/1.png\" alt=\"\" draggable=\"false\">\n" +
-    "                <p><span id=\"more\">Sed dapibus pulvinar nibh tempor portsdfsdfsdfsdfsdfsdfsdfsdfsfa.</span></p>\n" +
-    "                <button onclick=\"myFunction()\" id=\"myBtn\">Читать больше</button>\n" +
+    "                <p><span id=\"more1\">This is the start page of the Currency Bot.</span></p>\n" +
+    "                <button onclick=\"myFunction(more1, myBtn1)\" id=\"myBtn1\">Show text</button>\n" +
     "            </a>";
 const secondItem = "            <a class=\"item\">\n" +
     "                <img src=\"img/2.png\" alt=\"\" draggable=\"false\">\n" +
-    "                <p>Lorerdiet<span id=\"dots\">...</span><span id=\"more\">Sed dapibus pulvinar nibh tempor porta.</span></p>\n" +
-    "                <button onclick=\"myFunction()\" id=\"myBtn\">Читать больше</button>\n" +
+    "                <p><span id=\"more2\">Description of the currency bot</span></p>\n" +
+    "                <button onclick=\"myFunction(more2, myBtn2)\" id=\"myBtn2\">Show text</button>\n" +
     "            </a>";
 const thirdItem = "            <a class=\"item\">\n" +
     "                <img src=\"img/3.png\" alt=\"\" draggable=\"false\">\n" +
-    "                <p>Lorerdiet<span id=\"dots\">...</span><span id=\"more\">Sed dapibus pulvinar nibh tempor porta.</span></p>\n" +
-    "                <button onclick=\"myFunction()\" id=\"myBtn\">Читать больше</button>\n" +
+    "                <p><span id=\"more3\">Example of the usage of the currency bot</span></p>\n" +
+    "                <button onclick=\"myFunction(more3, myBtn3)\" id=\"myBtn3\">Show text</button>\n" +
     "            </a>";
 
 const items = [firstItem, secondItem, thirdItem];
 
-
 document.querySelector('#show-images').onclick = executeMathResult;
 function executeMathResult() {
     document.getElementById('first_sample_items').innerHTML = items[0] + items[1] + items[2];
+    const more1 = document.getElementById("more1");
+    const myBtn1 = document.getElementById("myBtn1");
+    const more2 = document.getElementById("more2");
+    const myBtn2 = document.getElementById("myBtn2");
+    const more3 = document.getElementById("more3");
+    const myBtn3 = document.getElementById("myBtn3");
 }
 
-function myFunction(dots, moreText, btnText) {
-    const dots = document.getElementById("dots");
-    const moreText = document.getElementById("more");
-    const btnText = document.getElementById("myBtn");
+function myFunction(moreText, btnText) {
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Читать больше";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Читать меньше";
+    if (btnText.innerHTML === "Show text") {
+        btnText.innerHTML = "Hide text";
         moreText.style.display = "inline";
+    } else {
+        btnText.innerHTML = "Show text";
+        moreText.style.display = "none";
     }
 }
